6aafdab403cf1c0398ce2780c66866754f68929f
Tipulidae <axel.nystrom@gmail.com>
2019-09-07 16:16:34 +0200

    WIP

--- src/puzzle.elm
+++ src/puzzle.elm
@@ -3,9 +3,9 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, div, h1, text)
 import Svg.Attributes exposing (id)
-import TypedSvg exposing (image, defs, svg, g, use)
+import TypedSvg exposing (image, defs, svg, g, use, clipPath, rect)
 import TypedSvg.Attributes exposing (width, height, xlinkHref)
-import TypedSvg.Types exposing (num, px)
+import TypedSvg.Types exposing (num, px, ClipPath(..))
 import TypedSvg.Core exposing (Svg)
 
 -- MAIN
@@ -42,31 +42,11 @@ init : () -> (Model, Cmd Msg)
 init () =
     ( Model
         [ Piece 0 (Point 0 0) (Point 0 0) False
-        , Piece 1 (Point 0 0) (Point 0 0) False
-        , Piece 2 (Point 0 0) (Point 0 0) False
-        , Piece 3 (Point 0 0) (Point 0 0) False
+        , Piece 1 (Point 0 1) (Point 0 1) False
+        , Piece 2 (Point 1 0) (Point 1 0) False
+        , Piece 3 (Point 1 1) (Point 1 1) False
         ]
     , Cmd.none )
-
-
-puzzleImage =
-  image
-    [ id "puzzle-image"
-    , xlinkHref "../resources/kitten.png"
-    ]
-    []
-
-piece : Svg msg
-piece =
-    g []
-        [ use
-            [ TypedSvg.Attributes.x <| px <| toFloat 100
-            , TypedSvg.Attributes.y <| px <| toFloat 0
-            , xlinkHref <| "#puzzle-image"
-            --, TypedSvg.Attributes.clipPath <| clipPathRef x y
-            ]
-            []
-        ]
 
 
 -- UPDATE
@@ -104,9 +84,72 @@ view model =
           [ width <| num 1000
           , height <| num 1000
           ]
-          [ defs
-              []
-              [ puzzleImage ]
-            , piece
-          ]
+          ( constructJigsawSvg model )
     ]
+
+constructJigsawSvg : Model -> List (Svg msg)
+constructJigsawSvg model =
+    defs [] ( defineJigsawSvg model ) :: constructSvgPieces model.pieces
+
+constructSvgPieces : List Piece -> List (Svg msg)
+constructSvgPieces pieces =
+    List.map pieceToSvg pieces
+
+pieceToSvg : Piece -> Svg msg
+pieceToSvg piece =
+--  piece p.position.x p.position.y
+--
+--piece : Int -> Int -> Svg msg
+--piece x y =
+    g []
+        [ use
+            [ TypedSvg.Attributes.x <| px <| toFloat piece.position.x
+            , TypedSvg.Attributes.y <| px <| toFloat piece.position.y
+            , xlinkHref <| "#puzzle-image"
+            , TypedSvg.Attributes.clipPath <| clipPathRef piece.offset.x piece.offset.y
+            ]
+            []
+        ]
+
+
+defineJigsawSvg : Model -> List (Svg msg)
+defineJigsawSvg model =
+    definePuzzleImage :: definePieceClipPaths model
+
+definePuzzleImage : Svg msg
+definePuzzleImage =
+  image
+    [ id "puzzle-image"
+    , xlinkHref "../resources/kitten.png"
+    ]
+    []
+
+definePieceClipPaths : Model -> List (Svg msg)
+definePieceClipPaths model =
+    [ pieceClipPath 0 0
+    , pieceClipPath 0 1
+    , pieceClipPath 1 0
+    , pieceClipPath 1 1
+    ]
+
+
+pieceClipPath x y =
+    clipPath [ id <| pieceClipId x y ]
+        [ rect
+            [ id <| pieceOutlineId x y
+            , width <| px 100
+            , height <| px 100
+            , TypedSvg.Attributes.x <| px <| toFloat <| x * 100
+            , TypedSvg.Attributes.y <| px <| toFloat <| y * 100
+            ]
+            []
+        ]
+
+pieceOutlineId x y =
+    "piece-" ++ String.fromInt x ++ "-" ++ String.fromInt y ++ "-outline"
+
+pieceClipId x y =
+    "piece-" ++ String.fromInt x ++ "-" ++ String.fromInt y ++ "-clip"
+
+clipPathRef x y =
+    ClipPathFunc <| "url(#" ++ pieceClipId x y ++ ")"
