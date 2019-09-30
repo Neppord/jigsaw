db8eac973889ab21548847e41020627c4d77b5ec
Axel Nyström <axel.nystrom@gmail.com>
2019-09-30 14:54:04 +0200

    Changes Point from Int to Float
    This ultimately simplifies all the other calculations, and makes better
    way for working with WebGL.

--- src/Point.elm
+++ src/Point.elm
@@ -1,16 +1,27 @@
-module Point exposing (Point, sub, add, dot, dist, taxiDist, toString, randomPoint, randomPoints, randomPointsAndZ)
+module Point exposing (Point, sub, add, mulPointWise, dist, taxiDist, toIntString, randomPoint, randomPoints, xToPixel, yToPixel)
 
 import Random
-import Random.List exposing (shuffle)
 
 type alias Point =
-  { x: Int
-  , y: Int
+  { x: Float
+  , y: Float
   }
+
+toIntString : Point -> String
+toIntString p =
+  (String.fromInt <| floor p.x) ++ " " ++ (String.fromInt <| floor p.y)
 
 toString : Point -> String
 toString p =
-  (String.fromInt p.x) ++ " " ++ (String.fromInt p.y)
+  (String.fromFloat p.x) ++ " " ++ (String.fromFloat p.y)
+
+xToPixel : Point -> String
+xToPixel p =
+  (String.fromInt <| floor p.x) ++ "px"
+
+yToPixel : Point -> String
+yToPixel p =
+  (String.fromInt <| floor p.y) ++ "px"
 
 sub : Point -> Point -> Point
 sub a b =
@@ -20,32 +31,25 @@ add : Point -> Point -> Point
 add a b =
   Point (a.x + b.x) (a.y + b.y)
 
-dot : Point -> Point -> Point
-dot a b =
+mulPointWise : Point -> Point -> Point
+mulPointWise a b =
   Point (a.x * b.x) (a.y * b.y)
 
 dist : Point -> Point -> Float
 dist a b =
-  sqrt <| (toFloat b.x - toFloat a.x)^2 + (toFloat b.y - toFloat a.y)^2
+  sqrt <| (b.x - a.x)^2 + (b.y - a.y)^2
 
-taxiDist : Point -> Point -> Int
+taxiDist : Point -> Point -> Float
 taxiDist a b =
   abs (b.x - a.x) + abs (b.y - a.y)
 
 
-randomPoint : Int -> Int -> Int -> Int -> Random.Generator Point
+randomPoint : Float -> Float -> Float -> Float -> Random.Generator Point
 randomPoint xmin xmax ymin ymax =
   Random.map2 Point
-    (Random.int xmin xmax)
-    (Random.int ymin ymax)
+    (Random.float xmin xmax)
+    (Random.float ymin ymax)
 
-randomPoints : Int -> Int -> Int -> Int -> Int -> Random.Generator (List Point)
+randomPoints : Int -> Float -> Float -> Float -> Float -> Random.Generator (List Point)
 randomPoints n xmin xmax ymin ymax =
   Random.list n <| randomPoint xmin xmax ymin ymax
-
-randomPointsAndZ : Int -> Int -> Int -> Int -> Int -> Random.Generator (List Point, List Int)
-randomPointsAndZ n xmin xmax ymin ymax =
-  Random.map2
-    Tuple.pair
-    (randomPoints n xmin xmax ymin ymax)
-    (shuffle <| List.range 0 (n - 1))
