c25f690097968b175c2ed6aec95e5a57e74b0800
Samuel Ytterbrink <neppord@gmail.com>
2020-08-12 15:08:14 +0200

    Reformat file

--- src/SvgUtil.elm
+++ src/SvgUtil.elm
@@ -6,12 +6,7 @@ module SvgUtil exposing
     , pointsToSvg
     )
 
-import Edge
-    exposing
-        ( Bezier(..)
-        , Edge(..)
-        , Orientation(..)
-        )
+import Edge exposing (Bezier(..), Edge(..), Orientation(..))
 import Point exposing (Point)
 
 
