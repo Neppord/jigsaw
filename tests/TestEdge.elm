9ea566e461f60909045c0dfc606effb9bebac2cd
Samuel Ytterbrink <neppord@gmail.com>
2020-09-03 16:22:39 +0200

    Add testcase and cleanup old ones

--- tests/TestEdge.elm
+++ tests/TestEdge.elm
@@ -19,14 +19,15 @@ indexOfSuite =
             )
         , test "Pieces that share a edge has the same index " <|
             all
-                [ \_ -> Edge.indexOf North 3 3 4 |> equal (Edge.indexOf South 3 3 1)
-                , \_ -> Edge.indexOf East 3 3 4 |> equal (Edge.indexOf West 3 3 5)
-                , \_ -> Edge.indexOf South 3 3 4 |> equal (Edge.indexOf North 3 3 7)
-                , \_ -> Edge.indexOf West 3 3 4 |> equal (Edge.indexOf East 3 3 3)
-                , \_ -> Edge.indexOf North 3 3 4 |> equal 7
+                [ \_ -> Edge.indexOf North 3 3 4 |> equal 7
                 , \_ -> Edge.indexOf East 3 3 4 |> equal 3
                 , \_ -> Edge.indexOf South 3 3 4 |> equal 10
                 , \_ -> Edge.indexOf West 3 3 4 |> equal 2
+                , \_ -> 7 |> equal (Edge.indexOf South 3 3 1)
+                , \_ -> 3 |> equal (Edge.indexOf West 3 3 5)
+                , \_ -> 10 |> equal (Edge.indexOf North 3 3 7)
+                , \_ -> 2 |> equal (Edge.indexOf East 3 3 3)
+                , \_ -> Edge.indexOf East 3 3 0 |> equal 0
                 ]
         ]
     
