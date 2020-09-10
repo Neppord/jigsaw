7a1b39a88dac11f4318f25194e44c4243fc096ff
Samuel Ytterbrink <neppord@gmail.com>
2020-09-10 16:09:49 +0200

    Implement delete

--- src/KD.elm
+++ src/KD.elm
@@ -1,5 +1,6 @@
 module KD exposing
     ( Tree(..)
+    , delete
     , insert
     , singleton
     , smallest
@@ -9,6 +10,29 @@ module KD exposing
 type Tree k
     = Empty
     | Node (Tree k) k (Tree k)
+
+
+delete : comparable -> Tree comparable -> Tree comparable
+delete k tree =
+    case tree of
+        Empty ->
+            tree
+
+        Node smaller k1 larger ->
+            case compare k k1 of
+                EQ ->
+                    case smallest larger of
+                        Just a ->
+                            Node smaller a (delete a larger)
+
+                        Nothing ->
+                            smaller
+
+                LT ->
+                    Node (delete k smaller) k1 larger
+
+                GT ->
+                    Node smaller k1 (delete k larger)
 
 
 smallest : Tree k -> Maybe k
--- tests/TestKD.elm
+++ tests/TestKD.elm
@@ -1,7 +1,7 @@
 module TestKD exposing (..)
 
 import Expect exposing (all, equal)
-import KD exposing (Tree(..), insert, singleton, smallest)
+import KD exposing (Tree(..), delete, insert, singleton, smallest)
 import Test exposing (Test, describe, test)
 
 
@@ -57,5 +57,42 @@ suite =
                         |> insert 1
                         |> smallest
                         |> equal (Just 1)
+            ]
+        , describe "delete"
+            [ test "it cant delete something that is not there" <|
+                \_ ->
+                    Empty
+                        |> delete 1
+                        |> equal Empty
+            , test "it does not remove items that dont match the given key" <|
+                \_ ->
+                    singleton 1
+                        |> delete 2
+                        |> equal (singleton 1)
+            , test "it removes the value if its the only value" <|
+                \_ ->
+                    singleton 1
+                        |> delete 1
+                        |> equal Empty
+            , test "it removes from smaller subtree if value is smaller" <|
+                \_ ->
+                    Node (singleton 1) 2 Empty
+                        |> delete 1
+                        |> equal (singleton 2)
+            , test "it removes from larger subtree if value is larger" <|
+                \_ ->
+                    Node Empty 2 (singleton 3)
+                        |> delete 3
+                        |> equal (singleton 2)
+            , test "it replaces root element with smaller from larger" <|
+                \_ ->
+                    Node Empty 2 (singleton 3)
+                        |> delete 2
+                        |> equal (singleton 3)
+            , test "it replaces root element smaller branch if larger is empty" <|
+                \_ ->
+                    Node (singleton 1) 2 Empty
+                        |> delete 2
+                        |> equal (singleton 1)
             ]
         ]
