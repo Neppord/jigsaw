e33d28bf38d3e7982b92c96815486e757074501d
Samuel Ytterbrink <neppord@gmail.com>
2020-09-11 13:13:18 +0200

    Add match so its easy to compare searches with values

---
+++ src/Match.elm
@@ -0,0 +1,61 @@
+module Match exposing (Match(..), compareWithMatch, match)
+
+{-| Match are used to create search criteria.
+
+  - match anything
+  - match smaller then including value
+  - match exact values
+  - match larger then including value
+  - match range of values including ends
+
+-}
+
+
+type Match k
+    = Anything
+    | EqualTo k
+    | SmallerThan k
+    | LargerThan k
+    | WithinRange k k
+
+
+match : Match comparable -> comparable -> Bool
+match m k =
+    compareWithMatch k m == EQ
+
+
+compareWithMatch : comparable -> Match comparable -> Order
+compareWithMatch k m =
+    case m of
+        Anything ->
+            EQ
+
+        EqualTo k1 ->
+            compare k k1
+
+        SmallerThan k1 ->
+            case compare k k1 of
+                GT ->
+                    GT
+
+                _ ->
+                    EQ
+
+        LargerThan k1 ->
+            case compare k k1 of
+                LT ->
+                    LT
+
+                _ ->
+                    EQ
+
+        WithinRange k1 k2 ->
+            let
+                res =
+                    compare k k1
+            in
+            if res == compare k k2 then
+                res
+
+            else
+                EQ
---
+++ tests/TestMatch.elm
@@ -0,0 +1,53 @@
+module TestMatch exposing (..)
+
+import Expect exposing (all, equal)
+import Match exposing (Match(..), compareWithMatch)
+import Test exposing (Test, describe, test)
+
+
+x =
+    compare
+
+
+suite : Test
+suite =
+    describe "CompareWithMatch"
+        [ test "everything is equal to anything" <|
+            \_ ->
+                compareWithMatch 1 Anything |> equal EQ
+        , test "Equal compares as normal" <|
+            all
+                [ \_ -> compareWithMatch 2 (EqualTo 3) |> equal LT
+                , \_ -> compareWithMatch 3 (EqualTo 3) |> equal EQ
+                , \_ -> compareWithMatch 4 (EqualTo 3) |> equal GT
+                ]
+        , test "smaller than is inclusive" <|
+            \_ -> compareWithMatch 3 (SmallerThan 3) |> equal EQ
+        , test "smaller than counts as equal if inside range" <|
+            \_ -> compareWithMatch 2 (SmallerThan 3) |> equal EQ
+        , test "smaller than counts as larger if outside range" <|
+            \_ -> compareWithMatch 4 (SmallerThan 3) |> equal GT
+        , test "larger than is inclusive" <|
+            \_ -> compareWithMatch 3 (LargerThan 3) |> equal EQ
+        , test "larger than counts as equal if inside range" <|
+            \_ -> compareWithMatch 4 (LargerThan 3) |> equal EQ
+        , test "larger than counts as smaller if outside range" <|
+            \_ -> compareWithMatch 2 (LargerThan 3) |> equal LT
+        , test "Withing range is inclusive" <|
+            all
+                [ \_ -> compareWithMatch 2 (WithinRange 2 4) |> equal EQ
+                , \_ -> compareWithMatch 4 (WithinRange 2 4) |> equal EQ
+                ]
+        , test "Withing range is equal if in range" <|
+            \_ -> compareWithMatch 3 (WithinRange 2 4) |> equal EQ
+        , test "Withing range is less than when less then both range stops" <|
+            all
+                [ \_ -> compareWithMatch 1 (WithinRange 4 2) |> equal LT
+                , \_ -> compareWithMatch 1 (WithinRange 2 4) |> equal LT
+                ]
+        , test "Withing range is greater than when greater then both range stops" <|
+            all
+                [ \_ -> compareWithMatch 5 (WithinRange 4 2) |> equal GT
+                , \_ -> compareWithMatch 5 (WithinRange 2 4) |> equal GT
+                ]
+        ]
